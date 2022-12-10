1fc9e41f7f19134cb50cd2396ff98d805061b9d3
leonkasovan <110372618+leonkasovan@users.noreply.github.com>
2022-12-10 16:37:15 +0700

    Update truetype.go

--- truetype.go
+++ truetype.go
@@ -2,7 +2,7 @@ package glfont
 
 import (
 	"fmt"
-	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/leonkasovan/gl/v3.1/gles2"
 	"github.com/golang/freetype"
 	"github.com/golang/freetype/truetype"
 	"golang.org/x/image/font"
